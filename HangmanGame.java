ad22257a383ded21c4bd5c3378a7bfb50075d438
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-04 11:14:09 -0800

    Implement basic gameplay loop in HangmanGame class

--- HangmanGame.java
+++ HangmanGame.java
@@ -5,6 +5,26 @@ public class HangmanGame
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        input.close();
+
+        int guesses = 6;
+
+        System.out.println("Enter the secret word: ");
+        String secretWord = input.nextLine();
+
+        while (guesses > 0)
+        {
+            System.out.println("Enter a letter: ");
+            char letter = input.next().charAt(0);
+
+            if (secretWord.indexOf(letter) != -1)
+            {
+                System.out.println("Correct!");
+            }
+            else
+            {
+                guesses--;
+                System.out.println("Incorrect! You have " + guesses + " guesses left.");
+            }
+        }
     }
 }
