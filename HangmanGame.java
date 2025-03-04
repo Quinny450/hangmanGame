1eb59f9f397be4c7210458d83142eb40dc07aac7
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-04 09:27:56 -0800

    Add .gitignore and import Scanner in HangmanGame class

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,7 +1,10 @@
-public HangmanGame()
+import java.util.Scanner;
+
+public class HangmanGame
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
